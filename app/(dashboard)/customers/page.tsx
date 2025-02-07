e968cae3fe01190d3a6a496f9ddfa2108dde6bca
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-02-07 17:11:43 -0600

    finally

---
+++ app/(dashboard)/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomersPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Customers</h1>
+            <p>Welcome to the customers page.</p>
+        </div>
+    );
+};
+
+export default CustomersPage;
